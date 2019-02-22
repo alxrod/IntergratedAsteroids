4264bd38b2b81a7273e4b758f0ff081c3b14b853
Alex <alexbrodriguez@gmail.com>
2019-02-21 21:48:20 -0500

    Updated so that now the polygons can be displayed on the page and mimic the randomly generated functions. Those can also be intergrated!

--- CalcJava/AlexCalc.java
+++ CalcJava/AlexCalc.java
@@ -1,25 +1,93 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
+import java.util.Random;
+import java.applet.*;
+import javax.swing.*;
+
 public class AlexCalc {
 //    Notes:
 //    area of sector of circle = 1/2 * theta * r^2
 //    so to approximate the polar integral
 
 //    you just make a really high n of sectors and add them up
-
     public double approxThreshhold = 10000;
+    public double visualizeThreshhold = 10000000;
 
     public double testFunc(double theta) {
 //        r = 2 + 4 * cos(theta)
         // Code the function you want to intergrate here and run the program!
-        return 4 - 2*Math.tan(theta);
+        return Math.tan(theta);
 
     }
 
     public double sectorArea(double r, double theta) {
         return 0.5 * theta * Math.pow(r,2);
+    }  
+
+    // We're going to have fixed operations with random variables
+
+    // So this is the parser of the function we'll use
+    public double funcParser(ArrayList parameters, double theta) {
+        // This is not concise code but I use it to make the equation more readable to the mathmatician
+        double a = (double)(parameters.get(0));
+        double b = (double)(parameters.get(1));
+        double c = (double)(parameters.get(2));
+        double d = (double)(parameters.get(3));
+        double f = (double)(parameters.get(4));
+        return (1.0/a) * Math.sin(b*theta + c) + d + f*Math.pow((theta-Math.PI), 2);
+
+    }
+
+    public Polygon funcVisualizer(ArrayList parameters, double lowerBound, double upperBound) {
+        int[] xPoints = new int[(int)visualizeThreshhold];
+        int[] yPoints = new int[(int)visualizeThreshhold];
+
+        double range = upperBound-lowerBound;
+        double jumpSize = range/visualizeThreshhold;
+        int count = 0;
+        for (double theta=lowerBound; theta<upperBound-jumpSize; theta+=jumpSize) {
+            double r = funcParser(parameters, theta);
+            double[] cartCoords = cartesianConvert(theta, r);
+            xPoints[count] = (int)Math.round(cartCoords[0]*10)+350;
+            yPoints[count] = (int)Math.round(cartCoords[1]*10)+350;
+            count++;
+        }
+
+        Polygon visualized = new Polygon(xPoints, yPoints, (int) visualizeThreshhold);
+        ArrayList<Double> bodies = new ArrayList<Double>();
+        return visualized;
+
+
+
+
+    }
+
+    public double[] cartesianConvert(double theta, double r) {
+        double[] cartCoords = new double[2];
+        cartCoords[0] = Math.cos(theta) * r;
+        cartCoords[1] = Math.sin(theta) * r;
+        return cartCoords;
+
+    }
+
+    public ArrayList generateFunc() {
+        ArrayList<Double> parameters = new ArrayList<Double>();
+        Random rand = new Random();
+        parameters.add(new Double(rand.nextInt(5)+1));
+        parameters.add(new Double(rand.nextInt(21)-10));
+        parameters.add(new Double(Math.PI).doubleValue());
+        parameters.add(new Double(rand.nextInt(10)+5));
+        parameters.add(Math.random()+0.01);
+        return parameters;
+
+
     }
 
 
-    public double calcIntegral(double lowerBound, double upperBound) {
+    public double calcIntegral(ArrayList parameters, double lowerBound, double upperBound) {
         double range = upperBound-lowerBound;
         double sum = 0;
         
@@ -29,8 +97,8 @@ public class AlexCalc {
 
         for(double n=lowerBound; n<(upperBound-theta); n += theta ){
             // System.out.println()
-            sum += sectorArea(testFunc(n), theta);
-            
+            sum += sectorArea(funcParser(parameters, n), theta);
+
         }
 
         return sum;
--- CalcJava/Tester.java
+++ CalcJava/Tester.java
@@ -1,9 +1,102 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
 
-class Tester { //2
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+class Tester implements Runnable { //2
+	final int WIDTH = 800;
+	final int HEIGHT = 800;
+
+	JFrame frame;
+	Canvas canvas;
+	BufferStrategy bufferStrategy;
+
+	public AlexCalc amath;
+	public Polygon funcVis;
+
    public static void main(String[] args) { //3
-      //Write Hello World to the terminal window
-      AlexCalc amath = new AlexCalc();
-      // System.out.println(amath.testFunc(0.5));
-      System.out.println(amath.calcIntegral((2*Math.PI)/3, (4*Math.PI)/3));
-   } //5
+
+      Tester ex = new Tester();
+	  new Thread(ex).start();
+   } 
+
+   public Tester() {
+   		// Testing:
+   		amath = new AlexCalc();
+	      // System.out.println(amath.testFunc(0.5));
+	    ArrayList func = amath.generateFunc();
+	    System.out.println(func);
+	      // Evalutation:
+	    System.out.println(amath.funcParser(func, (2*Math.PI/3)));
+
+	      // Integral:
+	    System.out.println(amath.calcIntegral(func, 0, 2*Math.PI));
+
+	    System.out.println(amath.cartesianConvert(2*Math.PI/3, 10)[0]+","+amath.cartesianConvert(2*Math.PI/3, 10)[1]);
+
+
+
+	    // Main:
+	    funcVis = amath.funcVisualizer(func, 0, 2*Math.PI);
+		frame = new JFrame("Basic Test Envirornment");
+
+		JPanel panel = (JPanel) frame.getContentPane();
+		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		panel.setLayout(null);
+
+		canvas = new Canvas();
+		canvas.setBounds(0, 0, WIDTH, HEIGHT);
+		canvas.setIgnoreRepaint(true);
+
+		panel.add(canvas);
+
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setVisible(true);
+
+
+		canvas.createBufferStrategy(2);
+		bufferStrategy = canvas.getBufferStrategy();
+
+		canvas.requestFocus();
+	}// BasicGameApp()
+
+	public void run() {
+
+		while (true) {
+			// paint the graphics
+			render();
+
+			update();
+			//sleep
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+
+			}
+		}
+	}
+
+	public void update() {
+	
+	}
+
+	private void render() {
+		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+		g.clearRect(0, 0, WIDTH, HEIGHT);
+		g.draw(funcVis);
+		g.dispose();
+
+		bufferStrategy.show();
+	}
 } //6
